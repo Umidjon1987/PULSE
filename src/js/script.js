09ca0771d2caaaa437f3477ee639675c75eec10b
Umid <umidshermatov.muslim@gmail.com>
2021-12-20 14:18:47 +0600

    20_12_2021_2

--- src/js/script.js
+++ src/js/script.js
@@ -39,12 +39,24 @@ $(document).ready(function(){
 
     // Modal
 
-    $('[data-modal=consultation]').on('click', function(){
+    $('[data-modal=consultation]').on('click', function() {
         $('.overlay, #consultation').fadeIn('slow');
     });
-    $('.modal__close').on('click', function(){
+    
+    $('.modal__close').on('click', function() {
         $('.overlay, #consultation, #thanks, #order').fadeOut('slow');
     });
+
+    $('.button_mini').each(function(i){
+        $(this).on('click', function(){
+            $('#order .modal__descr').text($('.catalog-item__subtitle').eq(i).text());
+            $('.overlay, #order').fadeIn('slow');
+        })
+    });
+
+    $('#consultation-form').validate();
+    $('#consultation form').validate();
+    $('#order form').validate();
 
   });
 
